25c764d3740537a78803a6ba18e197204e20c799
Spark-Matt <22567240+Spark-Matt@users.noreply.github.com>
2017-10-20 22:57:10 -0500

    Ex3

--- maze.js
+++ maze.js
@@ -1,7 +1,7 @@
 var loser = false;  // whether the user has hit a wall
 
 window.onload = function() {
-    document.getElementById("start").onclick = startClick;
+
     document.getElementById("end").onmouseover = overEnd;
     let boundaries = document.querySelectorAll("div#maze div.boundary");
     for (var i = 0; i < boundaries.length; i++) {
