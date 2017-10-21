876092b03b95bc1db384a16496e7092c2f08f1a9
Spark-Matt <22567240+Spark-Matt@users.noreply.github.com>
2017-10-20 21:53:48 -0500

    Ex1

--- maze.js
+++ maze.js
@@ -1,34 +1,7 @@
-var loser = false;  // whether the user has hit a wall
-
 window.onload = function() {
-    $("start").onclick = startClick;
-    $("end").onmouseover = overEnd;
-    var boundaries = $$("div#maze div.boundary");
-    for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].onmouseover = overBoundary;
-    }
+    document.getElementById("boundary1").onmouseover = overBoundary;
 };
 
 function overBoundary() {
-    loser = true;
-    var boundaries = $$("div#maze div.boundary");
-    for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].addClassName("youlose");
-    }
-}
-
-function startClick() {
-    loser = false;
-    var boundaries = $$("div#maze div.boundary");
-    for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].removeClassName("youlose");
-    }
-}
-
-function overEnd() {
-    if(loser) {
-        alert("Sorry, you lost. :[");
-    } else {
-        alert("You win! :]");
-    }
+    document.getElementById("boundary1").style.backgroundColor = "#ff8888";
 }
